e1a35ac8a13f00c7acb54db5bb827a702036c83c
Probir Sarkar <probirsarkar855@gmail.com>
2023-05-28 18:47:42 +0530

    test

--- utils.js
+++ utils.js
@@ -1,39 +1,23 @@
 const checkForWin = (board) => {
   // Check for horizontal wins
   for (let i = 0; i < 3; i++) {
-    if (
-      board[i][0] === board[i][1] &&
-      board[i][1] === board[i][2] &&
-      board[i][0] !== "-"
-    ) {
+    if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== "-") {
       return board[i][0];
     }
   }
 
   // Check for vertical wins
   for (let i = 0; i < 3; i++) {
-    if (
-      board[0][i] === board[1][i] &&
-      board[1][i] === board[2][i] &&
-      board[0][i] !== "-"
-    ) {
+    if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== "-") {
       return board[0][i];
     }
   }
 
   // Check for diagonal wins
-  if (
-    board[0][0] === board[1][1] &&
-    board[1][1] === board[2][2] &&
-    board[0][0] !== "-"
-  ) {
+  if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== "-") {
     return board[0][0];
   }
-  if (
-    board[0][2] === board[1][1] &&
-    board[1][1] === board[2][0] &&
-    board[0][2] !== "-"
-  ) {
+  if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== "-") {
     return board[0][2];
   }
   // If no one wins
